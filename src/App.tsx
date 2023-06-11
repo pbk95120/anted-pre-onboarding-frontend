c42673d9eebc53e5efbf63646479d4f5202ae52a
Park <pbk95120@gmail.com>
2023-06-11 19:12:38 +0900

    feat: make basic home page

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signin" element={<SignInPage />} />
         <Route path="/todo" element={<TodoPage />} />
       </Routes>
     </BrowserRouter>
