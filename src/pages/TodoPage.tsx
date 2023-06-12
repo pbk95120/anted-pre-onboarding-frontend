686e1ee1ff43817b2d2759e69531ad1da52bb75d
Park <pbk95120@gmail.com>
2023-06-12 10:35:06 +0900

    feat: todo design

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,4 +1,51 @@
+import { useState } from "react";
+
 const TodoPage = () => {
-  return <></>;
+  const [todos, setTodos] = useState([]);
+  const [addList, setAddList] = useState("");
+
+  return (
+    <section className="bg-gray-50">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+              TodoList
+            </h1>
+            <div className="flex">
+              <input
+                data-testid="new-todo-input"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 mr-2 w-9/12"
+              />
+              <button
+                data-testid="new-todo-add-button"
+                className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2 w-3/12"
+              >
+                추가
+              </button>
+            </div>
+            <li>
+              <label>
+                <input type="checkbox" className="mr-2" />
+                <span className="inline-block mr-5 w-6/12">TODO 1</span>
+              </label>
+              <button
+                data-testid="modify-button"
+                className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 mr-2"
+              >
+                수정
+              </button>
+              <button
+                data-testid="delete-button"
+                className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2"
+              >
+                삭제
+              </button>
+            </li>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default TodoPage;
